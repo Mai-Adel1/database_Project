8d9e33b9bd69fc657f053044e3480a09b71d9c98
Mai-Adel1 <mai196079@feng.bu.edu.eg>
2023-04-29 22:49:05 +0300

    modify filess

--- p1.py
+++ p1.py
@@ -8,14 +8,6 @@ from tkinter import messagebox as messagebox
 import fun
 
 
-# mydp= mysql.connector.connect( 
-#     host='database-1.cfo6vewprtg5.eu-north-1.rds.amazonaws.com',
-#     user='admin',
-#     password='0123456789',
-#     database='student_enroll'
-# )
-
-# mycursor= mydp.cursor()
 
 
 window=tk.Tk()
