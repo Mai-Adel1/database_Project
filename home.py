a76e16bd5db5bc41312e350f38bca627044d93d3
Mai-Adel1 <mai196079@feng.bu.edu.eg>
2023-05-06 20:28:44 +0300

    clean the code

--- home.py
+++ home.py
@@ -6,14 +6,10 @@ from support import clear_window
 def home(window):
     
     Label1=tk.Label(text='Welcome',**wel_label_txt)
-<<<<<<< HEAD
     Label1.place(x=200,y=100)
-=======
-    Label1.place(x=345,y=100)
->>>>>>> 26c72ff60a2ff406982a40de356b6e6a44c5e17c
 
     emp_btn=tk.Button(text='Continue',**nrom_btn,command=lambda:[clear_window(window),emp_win(window,home)])
-    emp_btn.place(x=320,y=500)
+    emp_btn.place(x=300,y=500)
 
 def start():
     window=tk.Tk()
--- setting.py
+++ setting.py
@@ -1,25 +1,15 @@
 label_txt ={
-<<<<<<< HEAD
     "font":('monospace',20,'bold'),
-=======
-    "font":"('Courier',25,'bold')",
->>>>>>> 26c72ff60a2ff406982a40de356b6e6a44c5e17c
     "fg":'white',
-    "bg":'#3A7FF6',
-    "justify": 'center'
+    "bg":'#EC7063'
 }
 wel_label_txt={
-<<<<<<< HEAD
     "font":('monospace',90,'bold'),
-=======
-    "font":"('Courier',2000,'bold')",
->>>>>>> 26c72ff60a2ff406982a40de356b6e6a44c5e17c
     "fg":'white',
-    "bg":'#3A7FF6'
+    "bg":'#EC7063'
 }
 nrom_btn = {
 
-<<<<<<< HEAD
     "font":('monospace',20,'bold'),
     "fg":'#EC7063',
     "bg":'#FDEBD0', 
@@ -32,34 +22,20 @@ back_btn = {
     "bg":"#EC7063", 
     "width":"5",
     "activeforeground":"#EC7063"
-=======
-    "font":"('Courier',20,'bold')",
-    "fg":'white',
-    "bg":'#3A7FF6', 
-    "width":"13"
-}
-back_btn = {
-
-    "font":"('Courier',25,'bold')",
-    "fg":"white",
-    "bg":"#3A7FF6", 
-    "width":"13",
-    "activeforeground":"white"
->>>>>>> 26c72ff60a2ff406982a40de356b6e6a44c5e17c
  
 }
 
 win = {
 
     "geometry":"800x600",
-    "background":'white'
+    "background":'#EC7063'
 
        }
 
 radio_btn ={
 
-"background" : "white",
+"background" : "#EC7063",
 "foreground" : "#000000", 
-"font":"('Courier',15,'bold')",
+"font":"('monospace',15,'bold')",
 
 }
