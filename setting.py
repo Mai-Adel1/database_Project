26c72ff60a2ff406982a40de356b6e6a44c5e17c
peter0124 <82339024+peter0124@users.noreply.github.com>
2023-05-06 17:16:13 +0300

    Add files via upload

--- setting.py
+++ setting.py
@@ -1,41 +1,42 @@
 label_txt ={
-    "font":"('monospace',25,'bold')",
+    "font":"('Courier',25,'bold')",
     "fg":'white',
-    "bg":'#EC7063'
+    "bg":'#3A7FF6',
+    "justify": 'center'
 }
 wel_label_txt={
-    "font":"('monospace',1000,'bold')",
+    "font":"('Courier',2000,'bold')",
     "fg":'white',
-    "bg":'#EC7063'
+    "bg":'#3A7FF6'
 }
 nrom_btn = {
 
-    "font":"('monospace',20,'bold')",
-    "fg":'#EC7063',
-    "bg":'#FDEBD0', 
+    "font":"('Courier',20,'bold')",
+    "fg":'white',
+    "bg":'#3A7FF6', 
     "width":"13"
 }
 back_btn = {
 
-    "font":"('monospace',20,'bold')",
+    "font":"('Courier',25,'bold')",
     "fg":"white",
-    "bg":"#EC7063", 
-    "width":"5",
-    "activeforeground":"#EC7063"
+    "bg":"#3A7FF6", 
+    "width":"13",
+    "activeforeground":"white"
  
 }
 
 win = {
 
     "geometry":"800x600",
-    "background":'#EC7063'
+    "background":'white'
 
        }
 
 radio_btn ={
 
-"background" : "#EC7063",
+"background" : "white",
 "foreground" : "#000000", 
-"font":"('monospace',15,'bold')",
+"font":"('Courier',15,'bold')",
 
 }
